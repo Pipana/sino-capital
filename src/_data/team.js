fc308b4c6430ec8d54c38f859b10abd5b162dea4
Fran Swan <71414784+franswan@users.noreply.github.com>
2021-08-08 15:05:08 -0700

    add partial team data

---
+++ src/_data/team.js
@@ -0,0 +1,17 @@
+module.exports = {
+    members: [
+        {
+          name: "Matthew Graham",
+          title: "CEO",
+          img: "/assets/img/team/mathew-graham-ceo-sino-capital.jpg",
+          bio: "Matthew Graham, CEO of Sino Global Capital, has seven years of mainland China investment banking experience with a focus on representing international technology companies in China for strategic partnership and investment. Matthew has been involved in blockchain since 2013, first slowly, then all at once. As Managing Partner of Sino Global&#x27;s Liquid Value blockchain fund Mr. Graham invests in blockchain technology with strategic relevance for China."
+        },
+        {
+          name: "Haru Chen",
+          title: "Partner",
+          img: "/assets/img/team/",
+          bio: "Haru Chen is a serial entrepreneur and investor from China’s Yunnan Province. She has been focused on blockchain since 2015, and has invested in numerous successful projects including Ethereum, Neo, Ontology, EOS, Ada, RSK, and AE."
+        }
+      ]
+  };
+  
